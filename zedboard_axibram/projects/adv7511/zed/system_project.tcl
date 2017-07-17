7ce3f9200f8c402c1e6559167f4fb925d38e7851
msyksphinz <msyksphinz.dev@gmail.com>
2017-07-18 01:37:19 +0900

    Func: update system_project.tcl for adv7511/zed

--- zedboard_axibram/projects/adv7511/zed/system_project.tcl
+++ zedboard_axibram/projects/adv7511/zed/system_project.tcl
@@ -13,4 +13,19 @@ adi_project_files adv7511_zed [list \
 
 adi_project_run adv7511_zed
 
-
+# Add IPs
+update_compile_order -fileset sources_1
+open_bd_design {./adv7511_zed.srcs/sources_1/bd/system/system.bd}
+update_ip_catalog -rebuild -repo_path ./../../../../ip/blockram_test_1.0
+startgroup
+create_bd_cell -type ip -vlnv user.org:user:blockram_test:1.0 blockram_test_0
+endgroup
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 \
+	-config {Master "/sys_ps7/M_AXI_GP0" intc_ip "Auto" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  \
+	[get_bd_intf_pins blockram_test_0/S00_AXI]
+regenerate_bd_layout
+reset_run synth_1
+reset_run system_xbar_0_synth_1
+launch_runs impl_1 -to_step write_bitstream -jobs 2
+wait_on_run impl_1
+save_bd_design
