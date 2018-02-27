48438991ebe52cea1809959883ebd048930faf16
msyksphinz <msyksphinz.dev@gmail.com>
2018-02-27 20:13:36 +0900

    Func: update environment

--- risc-v/tilelink/common.mk
+++ risc-v/tilelink/common.mk
@@ -1,2 +1,5 @@
+src_path := src/main/scala
+default_submodules := . chisel3 hardfloat
+
 chisel_srcs := $(foreach submodule,$(default_submodules) $(ROCKETCHIP_ADDONS),$(shell find $(base_dir)/$(submodule)/$(src_path) -name "*.scala"))
 
