612b65c0c6fecc8717afff3ce28f327debdc828e
msyksphinz <msyksphinz_dev.gmail.com>
2019-02-10 22:28:42 +0900

    Func: add cpu_hls directory

---
+++ vivado_hls/cpu_hls/directives.tcl
@@ -0,0 +1,3 @@
+set_directive_interface -mode s_axilite -bundle slv0 "cpu_hls"
+set_directive_interface -mode s_axilite -bundle slv0 "cpu_hls" inst_mem
+set_directive_interface -mode s_axilite -bundle slv0 "cpu_hls" data_mem
---
+++ vivado_hls/cpu_hls/test_cpu_hls.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "cpu_hls.h"
+
+int main ()
+{
+  uint32_t image_in[1024];
+  uint32_t image_out[1024];
+  cpu_hls (image_in, image_out);
+
+#ifdef C_SIMULATION
+  int x;
+  for(x = 0; x < 1024; x++) {
+    printf ("%08x\n", image_out[x]);
+  }
+#endif // C_SIMULATION
+
+  return 0;
+}
