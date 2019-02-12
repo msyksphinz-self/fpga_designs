3d979965f6e96d675825c8ee21692d2a9c5ee9b1
msyksphinz <msyksphinz_dev.gmail.com>
2019-02-12 14:25:08 +0900

    Func: replace typedef

--- vivado_hls/cpu_hls/rv32_cpu.hpp
+++ vivado_hls/cpu_hls/rv32_cpu.hpp
@@ -14,32 +14,36 @@ typedef enum {
 } memtype_t;
 
 class rv32_cpu {
-  uint32_t m_reg32[32];
+  typedef uint32_t XLEN_t   ;
+  typedef uint8_t  RegAddr_t;
+  typedef uint32_t INST_t   ;
+
+  XLEN_t m_reg32[32];
 
   uint32_t *m_data_mem;
 
 public:
   rv32_cpu (uint32_t *data_mem);
 
-  uint32_t mem_access (memtype_t op, uint32_t data, uint32_t addr, uint32_t *data_mem);
+  XLEN_t mem_access (memtype_t op, uint32_t data, uint32_t addr, uint32_t *data_mem);
 
-  inst_rv32_t decode_inst (uint32_t inst);
+  inst_rv32_t decode_inst (INST_t inst);
 
-  uint8_t get_rs1_addr (uint32_t inst) {
+  RegAddr_t get_rs1_addr (INST_t inst) {
     return ((inst >> 15) & 0x1f);
   }
-  uint8_t get_rs2_addr (uint32_t inst) {
+  RegAddr_t get_rs2_addr (INST_t inst) {
     return ((inst >> 20) & 0x1f);
   }
-  uint8_t get_rd_addr (uint32_t inst) {
+  RegAddr_t get_rd_addr (INST_t inst) {
     return ((inst >> 7) & 0x1f);
   }
 
-  uint32_t read_reg(uint8_t addr) {
+  XLEN_t read_reg(RegAddr_t addr) {
     return m_reg32[addr];
   }
 
-  void write_reg(uint8_t addr, uint32_t data) {
+  void write_reg(RegAddr_t addr, XLEN_t data) {
     m_reg32[addr] = data;
   }
 };
