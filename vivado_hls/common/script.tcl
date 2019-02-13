df8218abb36a5151d235902031707a77a2b10bea
msyksphinz <msyksphinz_dev.gmail.com>
2019-02-13 12:58:42 +0900

    Func: fix script

--- vivado_hls/common/script.tcl
+++ vivado_hls/common/script.tcl
@@ -3,11 +3,20 @@ set_top $env(HLS_TARGET)
 add_files $env(HLS_SRC)
 add_files -tb test_$env(HLS_TARGET).cpp -cflags "-DC_SIMULATION"
 open_solution $env(HLS_SOLUTION)
-# set_part {xcvu440-flgb2377-3-e}
-set_part {xc7z020clg484-1}
+
 create_clock -period 10 -name default
 source "./directives.tcl"
 csim_design
+
 csynth_design
+
+set fpga_parts [get_part]
+set vu440 [lsearch $fpga_parts xcvu440-flgb2377-3-e]
+if { $vu440 != -1 } {
+    set_part {xcvu440-flgb2377-3-e}
+} else {
+    set_part {xc7z020clg484-1}
+}
+
 # cosim_design
 export_design -format ip_catalog
