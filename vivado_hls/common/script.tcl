66cad9310ed5398f5d8ca24d216e2377c3d133f3
msyksphinz <msyksphinz_dev.gmail.com>
2019-02-15 22:55:33 +0900

    Func: update script for Zynq

--- vivado_hls/common/script.tcl
+++ vivado_hls/common/script.tcl
@@ -8,10 +8,10 @@ create_clock -period 10 -name default
 source "./directives.tcl"
 csim_design
 
-set fpga_parts [list_part virtexu]
-set vu440 [lsearch $fpga_parts xcvu440-flgb2377-3-e]
+set fpga_parts [list_part zynq]
+set zynq [lsearch $fpga_parts xc7z020clg484-1]
 
-if { $vu440 != -1 } {
+if { $zynq == -1 } {
     puts "Set VU440 as parts"
     set_part {xcvu440-flgb2377-3-e}
 } else {
